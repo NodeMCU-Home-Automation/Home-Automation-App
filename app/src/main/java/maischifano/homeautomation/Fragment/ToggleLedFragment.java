b97b872b6f856ca488dc057972e5c6d6d67acf60
Dani <dani.schifano@web.de>
2018-11-11 14:52:27 +0100

    Projekt erstellt
    Als erster Schritt das Android Studio Projekt erstellt.
    
    Ebenfalls mal den ersten test über dummy server.
    
    LED lässt sich auf mC ein bzw aus schalten

---
+++ app/src/main/java/maischifano/homeautomation/Fragment/ToggleLedFragment.java
@@ -0,0 +1,49 @@
+package maischifano.homeautomation.Fragment;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import org.springframework.web.client.RestTemplate;
+
+import maischifano.homeautomation.R;
+
+public class ToggleLedFragment extends Fragment {
+    private Button changeLedState;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstance) {
+        View toggleLEDView = inflater.inflate(R.layout.fragment_toggle_led, container, false);
+        changeLedState = toggleLEDView.findViewById(R.id.changeLedState);
+
+        changeLedState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ToggleLedOnEsp().execute();
+            }
+        });
+
+        return toggleLEDView;
+    }
+
+
+    private class ToggleLedOnEsp extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            RestTemplate restTemplate = new RestTemplate();
+            try {
+                restTemplate.put("http://192.168.178.22:8080/automatischeAntwort", "asd", String.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+}
